8418b53321390a12ec3e839d9e0887952e555d75
Ardi Nasiri <ardashir.nasirabadi@gmail.com>
2019-05-21 16:47:42 +0200

    Updates

---
+++ precache-manifest.58b28f396565e4fdfb8ddf0746e8ac00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39085fb88bab0ca565193177b30aa9f9",
+    "url": "/static/media/spinner.39085fb8.gif"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f567ad2d112fc78dd52",
+    "url": "/static/js/main.4a676837.chunk.js"
+  },
+  {
+    "revision": "ab6cfdc5e9311522abda",
+    "url": "/static/js/2.ca50c93b.chunk.js"
+  },
+  {
+    "revision": "0f567ad2d112fc78dd52",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "537806a72408b34760d84c7c2f34be2a",
+    "url": "/index.html"
+  }
+];
